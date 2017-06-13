137a4cd85101dcee2db3008d10db7b69b640fe0c
Kirill Rodriguez <kirill.rodriguez@codeplay.com>
2017-06-13 11:14:22 +0100

    added scal test

--- test/unittest/blas1_scal_test.cpp
+++ test/unittest/blas1_scal_test.cpp
@@ -10,30 +10,28 @@ REGISTER_PREC(float, 1e-4, axpy_test)
 REGISTER_PREC(double, 1e-6, axpy_test)
 REGISTER_PREC(long double, 1e-7, axpy_test)
 
-B1_TEST(axpy_test) {
+B1_TEST(scal_test) {
   UNPACK_PARAM(axpy_test);
   size_t size = TEST_SIZE;
   ScalarT prec = TEST_PREC;
 
   ScalarT alpha((rand() % size * 1e2) * 1e-2);
   std::vector<ScalarT> vX(size);
-  std::vector<ScalarT> vY(size);
-  std::vector<ScalarT> vZ(size, 0);
+  std::vector<ScalarT> vY(size, 0);
   TestClass::set_rand(vX, size);
-  TestClass::set_rand(vY, size);
 
   for (auto &d : cl::sycl::device::get_devices()) {
-    for (size_t i = 0; i < size; ++i) vZ[i] = alpha * vX[i] + vY[i];
+    for (size_t i = 0; i < size; ++i)
+      vY[i] = alpha * vX[i];
 
     auto q = TestClass::make_queue(d);
     Executor<ExecutorType> ex(q);
     {
       auto buf_vX = TestClass::make_buffer(vX);
-      auto buf_vY = TestClass::make_buffer(vY);
       auto view_vX = TestClass::make_vview(buf_vX);
-      auto view_vY = TestClass::make_vview(buf_vY);
-      _axpy(ex, size, alpha, view_vX, 1, view_vY, 1);
+      _scal(ex, size, alpha, view_vX, 1);
     }
-    for (size_t i = 0; i < size; ++i) ASSERT_NEAR(vZ[i], vY[i], prec);
+    for (size_t i = 0; i < size; ++i)
+      ASSERT_NEAR(vY[i], vX[i], prec);
   }
 }
